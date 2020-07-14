ae006f26809b7ef00b66842fad77cd258a48f2cf
Thibeylard <17107365+Thibeylard@users.noreply.github.com>
2020-07-14 11:03:51 +0200

    Mapped CurvePoint + constructors, getters, setters

--- Poseiden-skeleton/src/main/java/com/nnk/springboot/domain/CurvePoint.java
+++ Poseiden-skeleton/src/main/java/com/nnk/springboot/domain/CurvePoint.java
@@ -9,7 +9,74 @@ import java.sql.Timestamp;
 
 
 @Entity
-@Table(name = "curvepoint")
 public class CurvePoint {
-    // TODO: Map columns in data table CURVEPOINT with corresponding java fields
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+    private Integer curveId;
+    private Timestamp asOfDate;
+    private Double term;
+    private Double value;
+    private Timestamp creationDate;
+
+    public CurvePoint() {
+    }
+
+    public CurvePoint(Integer id,
+                      Integer curveId,
+                      Timestamp asOfDate,
+                      Double term,
+                      Double value,
+                      Timestamp creationDate) {
+        this.id = id;
+        this.curveId = curveId;
+        this.asOfDate = asOfDate;
+        this.term = term;
+        this.value = value;
+        this.creationDate = creationDate;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public Integer getCurveId() {
+        return curveId;
+    }
+
+    public void setCurveId(Integer curveId) {
+        this.curveId = curveId;
+    }
+
+    public Timestamp getAsOfDate() {
+        return asOfDate;
+    }
+
+    public void setAsOfDate(Timestamp asOfDate) {
+        this.asOfDate = asOfDate;
+    }
+
+    public Double getTerm() {
+        return term;
+    }
+
+    public void setTerm(Double term) {
+        this.term = term;
+    }
+
+    public Double getValue() {
+        return value;
+    }
+
+    public void setValue(Double value) {
+        this.value = value;
+    }
+
+    public Timestamp getCreationDate() {
+        return creationDate;
+    }
+
+    public void setCreationDate(Timestamp creationDate) {
+        this.creationDate = creationDate;
+    }
 }
