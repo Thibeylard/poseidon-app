1f3e2d77025480fa3e6f414051385988ba148d6f
Thibeylard <17107365+Thibeylard@users.noreply.github.com>
2020-07-20 11:15:50 +0200

    Progress on BidListControllerTest

--- Poseiden-skeleton/src/test/java/com/nnk/springboot/unit/controllers/BidListControllerTest.java
+++ Poseiden-skeleton/src/test/java/com/nnk/springboot/unit/controllers/BidListControllerTest.java
@@ -1,5 +1,48 @@
 package com.nnk.springboot.unit.controllers;
 
+import org.flywaydb.test.annotation.FlywayTest;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import static org.hamcrest.CoreMatchers.hasItem;
+import static org.hamcrest.Matchers.hasProperty;
+import static org.hamcrest.Matchers.is;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
 public class BidListControllerTest {
 
+    MockMvc mockMvc;
+
+    @Before
+    public void setup() {
+        mockMvc = MockMvcBuilders.standaloneSetup(new BidListControllerTest()).build();
+    }
+
+    @Test
+    @FlywayTest
+    public void getList() throws Exception {
+        this.mockMvc.perform(get("/bidList/list"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("list"))
+                .andExpect(model().attributeExists("allBids"));
+    }
+
+    @Test
+    @FlywayTest
+    public void update() throws Exception {
+        this.mockMvc.perform(get("/bidList/update/1"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("update"))
+                .andExpect(model().attribute("bidList", hasItem(
+                        hasProperty("id", is(1))
+                )));
+    }
 }
