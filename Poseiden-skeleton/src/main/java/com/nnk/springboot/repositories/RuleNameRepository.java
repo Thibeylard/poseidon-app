ceabcad0805d31870edde98def49e7b7b1a55a31
Thibeylard <17107365+Thibeylard@users.noreply.github.com>
2020-07-24 12:05:13 +0200

    Added SpringDataRest dependency + basic repository config

--- Poseiden-skeleton/src/main/java/com/nnk/springboot/repositories/RuleNameRepository.java
+++ Poseiden-skeleton/src/main/java/com/nnk/springboot/repositories/RuleNameRepository.java
@@ -2,7 +2,8 @@ package com.nnk.springboot.repositories;
 
 import com.nnk.springboot.domain.RuleName;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
-
+@RepositoryRestResource(collectionResourceRel = "rules", path = "rules")
 public interface RuleNameRepository extends JpaRepository<RuleName, Integer> {
 }
--- Poseiden-skeleton/src/main/java/com/nnk/springboot/repositories/TradeRepository.java
+++ Poseiden-skeleton/src/main/java/com/nnk/springboot/repositories/TradeRepository.java
@@ -2,7 +2,8 @@ package com.nnk.springboot.repositories;
 
 import com.nnk.springboot.domain.Trade;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
-
+@RepositoryRestResource(collectionResourceRel = "trades", path = "trades")
 public interface TradeRepository extends JpaRepository<Trade, Integer> {
 }
